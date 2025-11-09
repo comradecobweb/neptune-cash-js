701862872da69e66fbd578a30944e38a5779f1fa
comradecobweb <comradecobweb@gmail.com>
2025-11-09 19:59:05 +0100

    Export the library in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello Neptune Cash!');
+export * from "./NeptuneClient"
+
+export * from "./errors/RequestError"
+export * from "./errors/JSONRPCError"
+export * from "./errors/MethodNotFoundError"
+export * from "./errors/InvalidParamsError"
+
+export * from "./types/argument-types"
+export * from "./types/base-types"
+export * from "./types/response-types"
